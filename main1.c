73979545e2faa77ca1ac8be1ed09332d6440e72c
Abhinav R <abhinav.rangarajan17@gmail.com>
2025-02-17 15:12:06 +0530

    Create main1.c

---
+++ main1.c
@@ -0,0 +1,28 @@
+#include <stdio.h> #include <string.h>
+
+void removeDuplicates(char *str) { int index = 0; // Index to place unique characters
+
+for (int i = 0; str[i] != '\0'; i++) {
+    int found = 0;
+    for (int j = 0; j < index; j++) {
+        if (str[i] == str[j]) {
+            found = 1;
+            break;
+        }
+    }
+    if (!found) {
+        str[index++] = str[i];
+    }
+}
+str[index] = '\0'; // Null terminate the modified string
+
+}
+
+int main() { char str[100]; printf("Enter a string: "); scanf("%99s", str);
+
+removeDuplicates(str);
+printf("Output: %s\n", str);
+return 0;
+
+}
+
