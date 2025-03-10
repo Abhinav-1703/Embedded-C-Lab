a753607699364bf4ec487d4f02f45c7b591176d7
Abhinav R <abhinav.rangarajan17@gmail.com>
2025-03-10 14:47:41 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -3,28 +3,30 @@
 sbit switch_pin = P2^3;  // Define switch at P2.3
 
 void main() {
-    unsigned char data, count, i;
-    
+    unsigned char portValue;  
+    unsigned char count;  
+    unsigned char i;  
+
     P1 = 0x55;  // Load P1 with 0x55 (01010101 in binary)
-    
+
     while (1) {
-        data = P1;  // Read data from P1
+        portValue = P1;  // Read data from P1
         count = 0;
-        
+
         if (switch_pin == 1) {  // Switch ON -> Count 1s
             for (i = 0; i < 8; i++) {
-                if (data & (1 << i)) {
+                if (portValue & (1 << i)) {
                     count++;
                 }
             }
         } else {  // Switch OFF -> Count 0s
             for (i = 0; i < 8; i++) {
-                if (!(data & (1 << i))) {
+                if (!(portValue & (1 << i))) {
                     count++;
                 }
             }
         }
-        
-        // Store or display count as needed
+
+        P1 = count;  // Output the count value to Port 1
     }
 }
